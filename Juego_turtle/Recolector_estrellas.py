45efe11445aa09b6cecc83a4fab0466e83edf8a8
Esteban <91302321+EstebanGonzabay@users.noreply.github.com>
2025-06-26 23:21:22 -0500

    Update Recolector_estrellas.py

--- Juego_turtle/Recolector_estrellas.py
+++ Juego_turtle/Recolector_estrellas.py
@@ -3,8 +3,8 @@ import random
 
 #Creacion de pantalla del juego
 ventana = turtle.Screen()
-ventana = turtle.title("Recolector de estrellas")
-ventana = turtle.bgcolor("black")
+ventana.title("Recolector de estrellas")
+ventana.bgcolor("black")
 ventana.setup(width=600, height=600)
 
 
